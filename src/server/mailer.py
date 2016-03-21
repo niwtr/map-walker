2e01edd56a4529d72d74d308d5207c9b4e8bec6d
Herathol Nortzor <heranort@163.com>
2016-03-21 11:53:16 +0800

    written mailer, current version: 0.1

--- src/server/mailer.py
+++ src/server/mailer.py
@@ -1,62 +1,9 @@
 '''
 Mailer, a module communication protocol.
+Current version: 0.1 2016/3/21
 Design: Heranort
 '''
 
-
-'''
-We use mailbox to serve as a pipe(or interface, if you like) that connects 
-different modules. 
-Contains a list, in fact, a queue, that preserves mails sent by other modules.
-When a module (especially core module) was initialized, a mailbox should be 
-attached to it, when the module wants to listen to other modules, it checks 
-its maillist, get the message and do the behavior required. And when it wants
-to talk to other modules, it takes the send() method to send messages to that 
-module.
-
-This term of model ensures the uniqueness and clearness of interfaces between 
-any two modules. Besides, it allows tracking of message communication, which 
-is absolutely convenient for debugging and testing.
-
-##
-maillist: messages to be read,
-##
-
-%%
-________________________________________________________________________________
-read: read the first message before trashing and do the required behavior.
-
-ex. core.mailbox.read()
-________________________________________________________________________________
-send: push the required message to the mailbox of *other* module.
-
-ex. If the core wants to talk to the route module to calculate the shortest path,
- it does:
-route.mailbox.send(THE_MESSAGE_THAT_CALLS_FOR_SHORTEST_PATH_FUNCTION)
-**We'll explain the message later.**
-________________________________________________________________________________
-display_mails: display the messages got and do nothing.
-
-ex. core.mailbox.display_mails()
-________________________________________________________________________________
-%%
-
-Design: Heranort
-'''
-
-class mailbox():
-    
-    maillist=[]
-    
-        
-    def read(arg):
-        pass
-    
-    def send(msg_bubble):
-        pass
-    
-    def display_mails():
-        pass
 
 
 '''
@@ -97,7 +44,7 @@ def plus3(a,b,c):
     return a+b+c
 a_mail=mail("hello, world",plus3,1,2,3)
 
-a_mail.call()
+a_mail(c
 >>6
 a_mail.the_tag()
 >>"hello, world"
@@ -117,3 +64,94 @@ def mail(tag, func, *args):
             return call()
     return dispatch
 
+
+
+'''
+We use mailbox to serve as a pipe(or interface, if you like) that connects 
+different modules. 
+Contains a list, in fact, a queue, that preserves mails sent by other modules.
+When a module (especially core module) was initialized, a mailbox should be 
+attached to it, when the module wants to listen to other modules, it checks 
+its maillist, get the message and do the behavior required. And when it wants
+to talk to other modules, it takes the send() method to send messages to that 
+module.
+
+This term of model ensures the uniqueness and clearness of interfaces between 
+any two modules. Besides, it allows tracking of message communication, which 
+is absolutely convenient for debugging and testing.
+
+##
+maillist: messages to be read,
+##
+
+%%
+________________________________________________________________________________
+read: read the first message before trashing and do the required behavior.
+
+ex. core.mailbox.read()
+________________________________________________________________________________
+send: push the required message to the mailbox of *other* module.
+
+ex. If the core wants to talk to the route module to calculate the shortest path,
+ it does:
+core.mailbox.send(route.mailbox, FUNCTION_SHORTEST_PATH, "Changsha", "Wuhan")
+**The message was described above.**
+________________________________________________________________________________
+display_mails: display the messages got and do nothing.
+
+ex. core.mailbox.display_mails()
+________________________________________________________________________________
+read_all:read all the mails.
+________________________________________________________________________________
+%%
+
+Design: Heranort
+'''
+
+class mailbox():
+    
+    maillist=[]
+    mail_address=""
+    
+    def __init__(self, mail_address):
+        self.mail_address=mail_address
+        
+    def get_mail_address(self):
+        return self.mail_address
+
+    def read(self, arg=0):
+        if(len(self.maillist)!=0):
+            self.maillist.pop(0)(arg)
+
+    def read_all(self, args=[]):
+        for mail in self.maillist:
+            if (args!=[]):
+                mail(args.pop(0))
+            else:
+                mail()
+        self.maillist.clear()
+        
+    def get_mail(self,msg):    
+        self.maillist.append(msg)
+
+    def send(self,mailb,*msg):
+        mailb.get_mail(mail(self.get_mail_address(),*msg))
+
+    def display_mails(self):
+        for mail in self.maillist:
+            print ("Mail",mail,"from", mail('tag'))
+
+
+
+##testing.
+maila=mailbox('maila')
+mailb=mailbox('mailb')
+maila.send(mailb, print, "hello", "world1")
+maila.send(mailb, print, "hello", "world2")
+maila.send(mailb, print, "hello", "world3")
+maila.send(mailb, print, "hello", "world4")
+maila.send(mailb, print, "hello", "world5")
+mailb.display_mails()
+mailb.read()
+mailb.display_mails()
+mailb.read_all([1,'tag'])
