5aa5121c83780537a2b534f3e2537740ef383120
L.Laddie <liuhan132@foxmail.com>
2016-04-16 22:56:58 +0800

    Fix format of router

--- src/server/router.py
+++ src/server/router.py
@@ -22,14 +22,6 @@ from datab import database_binding
 import copy
 from log import log_file
 
-
-
-
-
-
-
-
-
 '''
 Write the current route to history.
 The history maybe used twice for reference of path recommendation.
@@ -38,91 +30,6 @@ The history maybe used twice for reference of path recommendation.
 def emit_to_history(route):
     pass
 
-
-
-'''
-################################################################################
-This algorithm can use mode to select different minimal ways.
-For example: minimal_path(data_all, 0, [1, 5, 9], 'price')
-################################################################################
-'''
-def minimal_path(datab_link, source, destination, mode):
-    dat_flight = datab_link['flight'][mode]
-    dat_train = datab_link['train'][mode]
-    dat_bus = datab_link['bus'][mode]
-    minimal = [[[-1 for i in range(2)] for i in range(10)] for i in range(10)]
-    inf = 100000   #set a infite value
-
-    #caluclate the minimal price matrix
-    for i in range(10):
-        for j in range(10):
-            a = inf
-            b = inf
-            c = inf
-            if(dat_flight[i][j]):
-                a = min(dat_flight[i][j])
-            if(dat_train[i][j]):
-                b = min(dat_train[i][j])
-            if(dat_bus[i][j]):
-                c = min(dat_bus[i][j])
-
-            if(a <= b and a <= c):   #the flight has lower price
-                minimal[i][j][0] = a
-                minimal[i][j][1] = 0
-            elif(b <= a and b <= c):  #the train has lower price
-                minimal[i][j][0] = b
-                minimal[i][j][1] = 1
-            else:                     #the bus has lower price
-                minimal[i][j][0] = c
-                minimal[i][j][1] = 2
-            if(minimal[i][j][0] == inf):  #regular the value
-                minimal[i][j] = [-1, -1]
-
-    #dijkstra algorithm
-    dis = [-1 for i in range(10)]
-    path = [[-1] for i in range(10)]
-    final = [False for i in range(10)]
-
-    for i in range(10):
-        dis[i] = minimal[source][i][0]
-        path[i] = [[source, minimal[source][i][1]]]
-    dis[source] = 0
-    final[source] = True
-    k = -1     #the last arrive city id
-    for i in range(9):
-        k = -1
-        min_temp = inf     #find the minimal dis[j]
-        for j in range(10):
-            if(final[j] == False and dis[j] < min_temp):
-                k = j
-                min_temp = dis[j]
-        final[k] = True      #add a city
-        if(k in destination):   #when a demand city added
-            destination.remove(k)
-            if(not destination):    #find all the demand city
-                break
-            for i in range(10):     #from the last demand city to find others
-                if(i != k):
-                    dis[i] = minimal[k][i][0] + dis[k]
-                    path[i] = copy.deepcopy(path[k])
-                    path[i].append([k, minimal[k][i][1]])
-        for w in range(10):   #from the select city to update others
-            if(final[w] == False and minimal[k][w][0] != -1 and (min_temp + minimal[k][w][0] < dis[w])):
-                dis[w] = min_temp + minimal[k][w][0]
-                path[w] = copy.deepcopy(path[k])
-                path[w].append([k, minimal[k][w][1]])
-    path[k].append([k, -1])  #add the last arrive city
-    return (path[k], dis[k])
-
-
-def minimal_cost_restricted(datab_link, source,destination):
-    
-    pass
-
-
-
-
-
 '''
 ################################################################################
 Work as interface toward the core module.
@@ -130,9 +37,9 @@ Exporting route_calculating functions used by core.
 Does not contian a mailbox.
 ################################################################################
 '''
-
 class router_module():  
     data_all=[]
+
     '''
     Initialize the router module, including initializing the mailer, ensuring conne-
     ction to the data base, checking the health of connection between core, and est-
@@ -140,23 +47,93 @@ class router_module():
     '''    
     def __init__(self, core_mail_binding, database_binding):
         self.data_all=database_binding.data_all
-        
 
     def minimal_cost_path(self,id_src, id_dest):
-        return minimal_path(self.data_all, id_src,id_dest, 'price')
+        return self.minimal_path(self.data_all, id_src,id_dest, 'price')
 
-    
     def minimal_time_path(self, id_src, id_dest):
-        return minimal_path(self.data_all, id_src, id_dest, 'time')
+        return self.minimal_path(self.data_all, id_src, id_dest, 'time')
 
-        
-    
-    
-    
-    
+
+    '''
+    ################################################################################
+    This algorithm can use mode to select different minimal ways.
+    For example: minimal_path(data_all, 0, [1, 5, 9], 'price')
+    ################################################################################
+    '''
+    def minimal_path(self, datab_link, source, destination, mode):
+        dat_flight = datab_link['flight'][mode]
+        dat_train = datab_link['train'][mode]
+        dat_bus = datab_link['bus'][mode]
+        minimal = [[[-1 for i in range(2)] for i in range(10)] for i in range(10)]
+        inf = 100000   #set a infite value
+
+        #caluclate the minimal price matrix
+        for i in range(10):
+            for j in range(10):
+                a = inf
+                b = inf
+                c = inf
+                if(dat_flight[i][j]):
+                    a = min(dat_flight[i][j])
+                if(dat_train[i][j]):
+                    b = min(dat_train[i][j])
+                if(dat_bus[i][j]):
+                    c = min(dat_bus[i][j])
+
+                if(a <= b and a <= c):   #the flight has lower price
+                    minimal[i][j][0] = a
+                    minimal[i][j][1] = 0
+                elif(b <= a and b <= c):  #the train has lower price
+                    minimal[i][j][0] = b
+                    minimal[i][j][1] = 1
+                else:                     #the bus has lower price
+                    minimal[i][j][0] = c
+                    minimal[i][j][1] = 2
+                if(minimal[i][j][0] == inf):  #regular the value
+                    minimal[i][j] = [-1, -1]
+
+        #dijkstra algorithm
+        dis = [-1 for i in range(10)]
+        path = [[-1] for i in range(10)]
+        final = [False for i in range(10)]
+
+        for i in range(10):
+            dis[i] = minimal[source][i][0]
+            path[i] = [[source, minimal[source][i][1]]]
+        dis[source] = 0
+        final[source] = True
+        k = -1     #the last arrive city id
+        for i in range(9):
+            k = -1
+            min_temp = inf     #find the minimal dis[j]
+            for j in range(10):
+                if(final[j] == False and dis[j] < min_temp):
+                    k = j
+                    min_temp = dis[j]
+            final[k] = True      #add a city
+            if(k in destination):   #when a demand city added
+                destination.remove(k)
+                if(not destination):    #find all the demand city
+                    break
+                for i in range(10):     #from the last demand city to find others
+                    if(i != k):
+                        dis[i] = minimal[k][i][0] + dis[k]
+                        path[i] = copy.deepcopy(path[k])
+                        path[i].append([k, minimal[k][i][1]])
+            for w in range(10):   #from the select city to update others
+                if(final[w] == False and minimal[k][w][0] != -1 and (min_temp + minimal[k][w][0] < dis[w])):
+                    dis[w] = min_temp + minimal[k][w][0]
+                    path[w] = copy.deepcopy(path[k])
+                    path[w].append([k, minimal[k][w][1]])
+        path[k].append([k, -1])  #add the last arrive city
+        return (path[k], dis[k])
+
+    def minimal_cost_restricted(self, datab_link, source,destination):
+        pass
+
     
 if(__name__ == '__main__'):
-
     pass
 
 
